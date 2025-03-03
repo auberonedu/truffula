b849efa5d6ed811f41e7d8a4e5c4d5f15c2789a8
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-03-02 19:53:32 -0800

    Added static import for assertThrows inside the TruffulaOptionsTest.java

--- src/TruffulaOptionsTest.java
+++ src/TruffulaOptionsTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
