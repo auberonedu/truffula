517333da701a1e5138b67573bfdd9be8fc768841
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-03-03 17:14:08 -0800

    Tried to debug WAVE 3 in progress

--- src/App.java
+++ src/App.java
@@ -45,15 +45,11 @@ public class App {
     // You should create a TruffulaOptions object using the args and
     // pass it to a new TruffulaPrinter that uses System.out
     // Then, call printTree on the TruffulaPrinter
+      TruffulaOptions options = new TruffulaOptions(args);
 
-    TruffulaOptions options = new TruffulaOptions(args); 
+      TruffulaPrinter prints = new TruffulaPrinter(options);
 
-    TruffulaPrinter prints = new TruffulaPrinter(options, System.out); 
-
-    prints.printTree(); 
-
-
-
-
+      prints.printTree();
+    
   }
 }
