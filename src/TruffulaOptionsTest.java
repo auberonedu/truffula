b2d157582ebf77a9a38ca4272a8c88192ec170de
Madeleine Chance <mechance782@gmail.com>
2025-03-04 16:54:48 -0800

    added extra tests for IllegalArgumentExceptions. Finished Wave 2

--- src/TruffulaOptionsTest.java
+++ src/TruffulaOptionsTest.java
@@ -79,6 +79,22 @@ public class TruffulaOptionsTest {
     assertTrue(options.isUseColor());
   }
 
+  @Test
+  void testFlagsSwitched(@TempDir File tempDir) throws FileNotFoundException {
+    // Arrange: Prepare the arguments with the temp directory
+    File directory = new File(tempDir, "subfolder");
+    directory.mkdir();
+    String directoryPath = directory.getAbsolutePath();
+    String[] args = { "-h", "-nc", directoryPath };
+
+    // Act: Create TruffulaOptions instance
+    TruffulaOptions options = new TruffulaOptions(args);
+
+    // Assert: Check that the root directory is set correctly
+    assertEquals(directory.getAbsolutePath(), options.getRoot().getAbsolutePath());
+    assertTrue(options.isShowHidden());
+    assertFalse(options.isUseColor());
+  }
   // Test that FileNotFoundException is thrown correctly
   @Test
   void testFileNotFoundExceptionisThrown(@TempDir File tempDir) {
@@ -95,7 +111,7 @@ public class TruffulaOptionsTest {
 
   // Test that IllegalArgumentException is thrown correctly
   @Test
-  void testIllegalArgumentException(@TempDir File tempDir) {
+  void testIllegalArgumentExceptionInvalidFlags(@TempDir File tempDir) {
     File directory = new File(tempDir, "subfolder");
     directory.mkdir();
 
@@ -108,4 +124,66 @@ public class TruffulaOptionsTest {
 
     Assertions.assertEquals("Invalid flags", exception.getMessage());
   }
+
+  @Test
+  void testIllegalArgumentExceptionDuplicateFlags(@TempDir File tempDir) {
+    File directory = new File(tempDir, "subfolder");
+    directory.mkdir();
+
+    String directoryPath = directory.getAbsolutePath();
+    String[] args = { "-h", "-h", directoryPath };
+
+    IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      TruffulaOptions options = new TruffulaOptions(args);
+    });
+
+    Assertions.assertEquals("Invalid flags", exception.getMessage());
+  }
+
+  @Test
+  void testIllegalArgumentExceptionSingleInvalidFlag(@TempDir File tempDir) {
+    File directory = new File(tempDir, "subfolder");
+    directory.mkdir();
+
+    String directoryPath = directory.getAbsolutePath();
+    String[] args = { "-g", directoryPath };
+
+    IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      TruffulaOptions options = new TruffulaOptions(args);
+    });
+
+    Assertions.assertEquals("Cannot recognize flag: -g", exception.getMessage());
+  }
+
+  @Test
+  void testIllegalArgumentExceptionEmptyArgs(@TempDir File tempDir) {
+    File directory = new File(tempDir, "subfolder");
+    directory.mkdir();
+
+    String directoryPath = directory.getAbsolutePath();
+    String[] args = { };
+
+    IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      TruffulaOptions options = new TruffulaOptions(args);
+    });
+
+    Assertions.assertEquals("Incorrect number of arguments", exception.getMessage());
+  }
+
+  @Test
+  void testIllegalArgumentExceptionTooManyArgs(@TempDir File tempDir) {
+    File directory = new File(tempDir, "subfolder");
+    directory.mkdir();
+
+    String directoryPath = directory.getAbsolutePath();
+    String[] args = { "-h", "-nc", "-h", directoryPath };
+
+    IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+      TruffulaOptions options = new TruffulaOptions(args);
+    });
+
+    Assertions.assertEquals("Incorrect number of arguments", exception.getMessage());
+  }
+
+  
 }
