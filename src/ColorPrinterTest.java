6f3657af577da04b296310c15145fc8fb197b2ed
Danny McCarragher <dannymccarragher@gmail.com>
2025-03-05 20:54:09 -0800

    Added unit test for print method with empty message

--- src/ColorPrinterTest.java
+++ src/ColorPrinterTest.java
@@ -46,4 +46,23 @@ class ColorPrinterTest {
     //Assert
     assertEquals(expectedOutput, outputStream.toString());
   }
+
+  @Test
+  void testPrintEmptyMessage() {
+     //Arrange
+     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+     PrintStream printStream = new PrintStream(outputStream);
+ 
+     ColorPrinter printer = new ColorPrinter(printStream);
+     printer.setCurrentColor(ConsoleColor.BLUE);
+ 
+     //Act
+     String message = "";
+     printer.print(message, true);
+
+     // Assert
+    String expectedOutput = ConsoleColor.BLUE.getCode() + ConsoleColor.RESET.getCode();
+    assertEquals(expectedOutput, outputStream.toString());
+  }
+
 }
