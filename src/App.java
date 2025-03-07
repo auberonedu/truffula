e750aea0ef060063ec1aa75d77183b57d398a973
f3liz <felixchen14@gmail.com>
2025-03-07 11:34:43 -0800

    wave 3 code

--- src/App.java
+++ src/App.java
@@ -45,5 +45,8 @@ public class App {
     // You should create a TruffulaOptions object using the args and
     // pass it to a new TruffulaPrinter that uses System.out
     // Then, call printTree on the TruffulaPrinter
+    TruffulaOptions truffula = new TruffulaOptions(args);
+    TruffulaPrinter printer = new TruffulaPrinter(truffula);
+    printer.printTree();
   }
 }
