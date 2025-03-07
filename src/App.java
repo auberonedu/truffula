16b6d3c39bfaf49f4d7fba9a859d5b7aeb92bc40
Alexander Ruban <alexruban29@gmail.com>
2025-03-07 13:07:18 -0800

    Implemented main method in app.java.

--- src/App.java
+++ src/App.java
@@ -45,5 +45,8 @@ public class App {
     // You should create a TruffulaOptions object using the args and
     // pass it to a new TruffulaPrinter that uses System.out
     // Then, call printTree on the TruffulaPrinter
+    
+    TruffulaPrinter printer = new TruffulaPrinter(new TruffulaOptions(args));
+    printer.printTree();
   }
 }
