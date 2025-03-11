0076920b5703d29145008b83e6f1b583cba03595
f3liz <felixchen14@gmail.com>
2025-03-11 12:36:51 -0700

    renamed test file

--- src/TruffulaPrinterTest.java
+++ src/TruffulaPrinterTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TruffulaPrinterHiddenFilesTest {
+public class TruffulaPrinterTest {
 
 
 @Test
