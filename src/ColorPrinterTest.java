5801f80597e8d25789cead18d3f76cc579745b22
Danny McCarragher <dannymccarragher@gmail.com>
2025-03-12 18:28:18 -0700

    Created a new test to check if reset is working properly

--- src/ColorPrinterTest.java
+++ src/ColorPrinterTest.java
@@ -100,4 +100,21 @@ class ColorPrinterTest {
     assertEquals(expectedOutput, outputStream.toString());
   }
 
+  @Test
+  void testPrintWithBlackandReset() {
+    //Arrange
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+
+    ColorPrinter printer = new ColorPrinter(printStream);
+    printer.setCurrentColor(ConsoleColor.BLACK);
+
+    String message = "This Message will be the color black";
+    printer.print(message, true);
+
+    // Assert
+    String expectedOutput = ConsoleColor.BLACK.getCode() + message + ConsoleColor.RESET;
+    assertEquals(expectedOutput, outputStream.toString());
+  }
+
 }
